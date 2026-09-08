914da5aec87f666670f8799ecf512ace79664708
Abhavya28 <abhavya2801@gmail.com>
2026-09-08 20:20:49 +0530

    added about and art

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import { ScrollTrigger, SplitText } from 'gsap/all'
 import Navbar from './components/navbar'
 import Hero from './components/hero'
 import Cocktails from './components/cocktails'
+import About from './components/about'
+import Art from './components/art'
 
 gsap.registerPlugin(ScrollTrigger, SplitText)
 const App = () => {
@@ -12,6 +14,8 @@ const App = () => {
       <Navbar />
       <Hero />
       <Cocktails />
+      <About />
+      <Art />
     </main>
   )
 }
